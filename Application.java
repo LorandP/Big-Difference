e7f7a3899ee772b8939fa5ab4befeda9b664d168
Lori <palfalvi.lorand@gmail.com>
2017-02-02 22:27:28 +0200

    Added verification if array is null or empty.

--- Application.java
+++ Application.java
@@ -7,12 +7,16 @@ import java.util.Arrays;
 public class Application {
     /**
      * This method is used to find the difference between the largest and the smallest number from the array.
+     *
      * @param nums the array.
      * @return the difference between the largest and smallest values in the array.
      */
     public int bigDiff(int[] nums) {
         int last_value = 0;
         int difference = 0;
+
+        if (nums == null || nums.length == 0)
+            return 0;
         Arrays.sort(nums);
 
         for (int counter = 0; counter < nums.length; counter++) {
@@ -25,7 +29,7 @@ public class Application {
 
     public static void main(String[] args) {
         Application application = new Application();
-        int[] nums = {2, 10, 7, 2};
+        int[] nums = {};
 
         System.out.println(application.bigDiff(nums));
     }
